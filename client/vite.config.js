e3ea31ab4bfc77ad3c23dd037e37d04902b6389e
ReachoutMarketing <reachoutmarketingsalem@gmail.com>
2025-05-23 16:06:14 +0530

    vite commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,17 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "ReachoutProject",
-
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:8800",
-        changeOrigin: true,
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    base: process.env.VITE_BASE_PATH || "/",
+    server: command === "serve" ? {
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: "http://localhost:8800",
+          changeOrigin: true,
+        },
       },
-    },
-  },
+    } : undefined,
+  };
 });
